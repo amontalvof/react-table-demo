21e2ff6b4816cdf7a61550991a75959391d39b92
amontalvof <amontalvof92@gmail.com>
2022-09-20 10:46:30 -0500

    feat: Header Groups

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,10 +1,11 @@
 import React, { useMemo } from 'react';
 import { useTable } from 'react-table';
 import MOCK_DATA from './MOCK_DATA.json';
-import { COLUMNS } from './columns';
+import { COLUMNS /*, GROUPED_COLUMNS*/ } from './columns';
 import './table.css';
 
 const BasicTable = () => {
+    // const columns = useMemo(() => GROUPED_COLUMNS, []);
     const columns = useMemo(() => COLUMNS, []);
     const data = useMemo(() => MOCK_DATA, []);
 
