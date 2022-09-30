c66d7983b9d993f33ab3e6de9fba60528221ab82
amontalvof <amontalvof92@gmail.com>
2022-09-30 09:30:50 -0500

    feat: Selecting Rows

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 // import BasicTable from './components/BasicTable';
 // import SortingTable from './components/SortingTable';
 // import FilteringTable from './components/FilteringTable';
-import PaginationTable from './components/PaginationTable';
+// import PaginationTable from './components/PaginationTable';
+import RowSelection from './components/RowSelection';
 
 function App() {
     return (
         <div className="App">
-            <PaginationTable />
+            <RowSelection />
         </div>
     );
 }
