6aea27eb6140034a4dce6377ad0d6d13fd56d21b
amontalvof <amontalvof92@gmail.com>
2022-10-02 12:41:12 -0500

    feat: Column Ordering

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 // import SortingTable from './components/SortingTable';
 // import FilteringTable from './components/FilteringTable';
 // import PaginationTable from './components/PaginationTable';
-import RowSelection from './components/RowSelection';
+// import RowSelection from './components/RowSelection';
+import ColumnOrder from './components/ColumnOrder';
 
 function App() {
     return (
         <div className="App">
-            <RowSelection />
+            <ColumnOrder />
         </div>
     );
 }
