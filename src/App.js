f12b9dd08afe751a15c7e5b7e91c5a99f003a668
amontalvof <amontalvof92@gmail.com>
2022-10-03 00:18:56 -0500

    feat: Column Hiding

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './App.css';
 // import FilteringTable from './components/FilteringTable';
 // import PaginationTable from './components/PaginationTable';
 // import RowSelection from './components/RowSelection';
-import ColumnOrder from './components/ColumnOrder';
+// import ColumnOrder from './components/ColumnOrder';
+import ColumnHiding from './components/ColumnHiding';
 
 function App() {
     return (
         <div className="App">
-            <ColumnOrder />
+            <ColumnHiding />
         </div>
     );
 }
